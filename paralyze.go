37e8571c1baa2d82ba39f2e9be9fd1bf4e98913b
Ian Lozinski <iml@uber.com>
2015-11-24 16:42:02 -0800

    Add Paralyze func

---
+++ paralyze.go
@@ -0,0 +1,41 @@
+package paralyzer
+
+import "sync"
+
+// Paralyze parallelizes a function and returns two slices.
+func Paralyze(funcs ...func() (interface{}, error)) ([]interface{}, []error) {
+	var wg sync.WaitGroup
+	results := make([]interface{}, len(funcs))
+	errors := make([]error, len(funcs))
+	for i, fn := range funcs {
+		wg.Add(1)
+		go func(i int, fn func() (chan interface{}, chan error)) {
+			defer wg.Done()
+			resCh, errCh := fn()
+			select {
+			case res := <-resCh:
+				results[i] = res
+			case err := <-errCh:
+				errors[i] = err
+			}
+		}(i, convert(fn))
+	}
+	wg.Wait()
+	return results, errors
+}
+
+func convert(fn func() (interface{}, error)) func() (chan interface{}, chan error) {
+	return func() (chan interface{}, chan error) {
+		resCh := make(chan interface{})
+		errCh := make(chan error)
+		go func() {
+			res, err := fn()
+			if err != nil {
+				errCh <- err
+			} else {
+				resCh <- res
+			}
+		}()
+		return resCh, errCh
+	}
+}
