684a33b06f3c59989031a67932cd5045421a1113
Jonah George <jonah.george@usebutton.com>
2020-02-25 17:10:05 -0500

    Increase test coverage

--- paralyze_test.go
+++ paralyze_test.go
@@ -13,9 +13,10 @@ import (
 var (
 	someError = errors.New("some error")
 
-	slowFn = func() (interface{}, error) { time.Sleep(time.Second); return "ok", nil }
-	fastFn = func() (interface{}, error) { return 55, nil }
-	errFn  = func() (interface{}, error) { return nil, someError }
+	slowFn  = func() (interface{}, error) { time.Sleep(time.Second); return "ok", nil }
+	fastFn  = func() (interface{}, error) { return 55, nil }
+	errFn   = func() (interface{}, error) { return nil, someError }
+	panicFn = func() (interface{}, error) { panic("whoops") }
 )
 
 func TestParalyze(t *testing.T) {
@@ -49,9 +50,54 @@ func TestParalyzeWithTimeout(t *testing.T) {
 	assert.Nil(t, results[2])
 
 	// Assert that errors are
-	assert.Error(t, errs[0])
+	assert.Equal(t, ErrTimedOut, errs[0])
 	assert.Nil(t, errs[1])
 	assert.Equal(t, someError, errs[2])
+}
+
+func TestParalyzeWithZeroTimeout(t *testing.T) {
+	results, errs := ParalyzeWithTimeout(0, slowFn, fastFn, errFn)
+
+	// Make sure both slices returned are the correct length
+	assert.Equal(t, 3, len(results))
+	assert.Equal(t, 3, len(errs))
+
+	// Assert that return values are in the correct order
+	assert.Equal(t, "ok", results[0])
+	assert.Equal(t, 55, results[1])
+	assert.Nil(t, results[2])
+
+	// Assert that errors are
+	assert.Nil(t, errs[0])
+	assert.Nil(t, errs[1])
+	assert.Equal(t, someError, errs[2])
+}
+
+func TestParalyzeWithCancel(t *testing.T) {
+	done := make(chan struct{})
+	go func() {
+		select {
+		case <-time.After(time.Millisecond):
+			done <- struct{}{}
+		}
+	}()
+
+	results, errs := ParalyzeWithCancel(done, slowFn, fastFn, errFn)
+
+	// Make sure both slices returned are the correct length
+	assert.Equal(t, 3, len(results))
+	assert.Equal(t, 3, len(errs))
+
+	// Assert that return values are in the correct order
+	assert.Nil(t, results[0])
+	assert.Equal(t, 55, results[1])
+	assert.Nil(t, results[2])
+
+	// Assert that errors are
+	assert.Equal(t, ErrCanceled, errs[0])
+	assert.Nil(t, errs[1])
+	assert.Equal(t, someError, errs[2])
+
 }
 
 func TestParalyzeWithCtx(t *testing.T) {
@@ -103,12 +149,7 @@ func fnCreator(wait time.Duration) ParalyzableCtx {
 
 func TestParalyzePanic(t *testing.T) {
 	assert.Panics(t, func() {
-		Paralyze(
-			func() (interface{}, error) {
-				panic("whoops")
-				return nil, nil
-			},
-		)
+		Paralyze(panicFn)
 	})
 }
 
